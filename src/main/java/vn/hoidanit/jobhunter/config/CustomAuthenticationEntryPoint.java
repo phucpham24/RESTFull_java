88ee6d27b2f567c1c0f902c18e8d3d28426f4e08
phuc pham <phamtrandaiphuchcmut@gmail.com>
2024-10-19 14:03:30 +0200

    refactor

--- src/main/java/vn/hoidanit/jobhunter/config/CustomAuthenticationEntryPoint.java
+++ src/main/java/vn/hoidanit/jobhunter/config/CustomAuthenticationEntryPoint.java
@@ -10,11 +10,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import vn.hoidanit.jobhunter.domain.response.RestResponse;
 
 import java.io.IOException;
 import java.util.Optional;
-
-import vn.hoidanit.jobhunter.domain.RestResponse;
 
 import org.springframework.http.HttpStatus;
 
