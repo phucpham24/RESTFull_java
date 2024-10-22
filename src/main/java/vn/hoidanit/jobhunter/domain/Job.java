47c99a3ea9f48de70c2a1e833d58dbce498cc323
phuc pham <phamtrandaiphuchcmut@gmail.com>
2024-10-22 10:03:57 +0200

    change job level to string

--- src/main/java/vn/hoidanit/jobhunter/domain/Job.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/Job.java
@@ -8,6 +8,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -43,9 +45,11 @@ public class Job {
     private String location;
     private double salary;
     private int quantity;
+
+    @Enumerated(EnumType.STRING)
     private GenderJobLevel level;
 
-    @Column(columnDefinition = "LONGTEXT")
+    @Column(columnDefinition = "MEDIUMTEXT")
     private String description;
 
     @Column(name = "start_date")
